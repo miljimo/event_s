c34834805b59e9c63884ae5b5cd66c80b160a865
Johnson Obaro Isreal <obaro.johnson@servotestsystems.com>
2019-10-12 17:00:06 +0100

    Setup updated

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from setuptools import setup;
 
 setup(
       name = "messaging",
-      version="0.1.0",
+      version="0.0.1",
       description="Event handling system using the pub/subscriber pattern",
-      url="https://github.com/miljimo/messaging",
+      url="https://github.com/miljimo/messaging.git",
       author="Obaro I. Johnson",
       author_email="johnson.obaro@hotmail.com",
       packages=["messaging",], 
       install_requires=['mpi4py>=2.0',
                        ],
       classifiers=[
-          "Users : Developers, Python Engineers",
-          "Target : Python 2 and Python 3"
+          "Users : Developers",
+          "Target : Python 2 and 3"
           ]);
 
       
